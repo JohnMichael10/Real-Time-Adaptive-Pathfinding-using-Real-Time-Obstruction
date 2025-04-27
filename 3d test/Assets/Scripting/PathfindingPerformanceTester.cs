1c5c61730b1dde33938a14d0118f6e20f7d113aa
PromiseSamManalang <126487199+PromiseSamManalang@users.noreply.github.com>
2025-04-27 13:57:58 +0800

    tester
    tester

---
+++ 3d test/Assets/Scripting/PathfindingPerformanceTester.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class PathfindingPerformanceTester : MonoBehaviour
+{
+    private NavAgent agent;
+    private bool isMeasuring = false;
+    private float startTime;
+    private long startMemory;
+
+    void Start()
+    {
+        agent = GetComponent<NavAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (agent != null && agent.destination != null)
+            {
+                StartMeasurement();
+            }
+        }
+
+        if (isMeasuring && agent != null && agent.IsMovementEnabled && HasReachedDestination())
+        {
+            EndMeasurement();
+        }
+    }
+
+    void StartMeasurement()
+    {
+        Debug.Log("<color=cyan>Starting pathfinding measurement...</color>");
+        startTime = Time.realtimeSinceStartup;
+        startMemory = Profiler.GetTotalAllocatedMemoryLong();
+        isMeasuring = true;
+    }
+
+    void EndMeasurement()
+    {
+        float endTime = Time.realtimeSinceStartup;
+        long endMemory = Profiler.GetTotalAllocatedMemoryLong();
+
+        float timeElapsed = endTime - startTime;
+        float memoryUsedMB = (endMemory - startMemory) / (1024f * 1024f);
+
+        Debug.Log("<b><color=lime>Pathfinding completed!</color></b>");
+        Debug.Log($"<color=yellow>Time Elapsed: {timeElapsed:F4} seconds</color>");
+        Debug.Log($"<color=orange>Memory Allocated: {memoryUsedMB:F4} MB</color>");
+
+        isMeasuring = false;
+    }
+
+    bool HasReachedDestination()
+    {
+        if (agent.destination == null) return false;
+        float distance = Vector3.Distance(transform.position, agent.destination.position);
+        return distance <= agent.destinationStopDistance + 0.05f; // Allow some margin
+    }
+}
